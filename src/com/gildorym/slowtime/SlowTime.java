758d32495ed4d36f5fd3d825b89baa1fbf7a2808
Hohahihehu <oaieuy@hotmail.com>
2013-08-29 11:51:04 -0700

    Fixed Typo
    Forgot a )

--- src/com/gildorym/slowtime/SlowTime.java
+++ src/com/gildorym/slowtime/SlowTime.java
@@ -13,7 +13,7 @@ public class SlowTime extends JavaPlugin {
 				// Regular Minecraft speed is 1 MC Day / 20 IRL min
 				// This plugin sets time equal to 5/72 of Regular Minecraft Speed
 				// Modified Minecraft speed becomes 5 MC Days / 1440 IRL min (1 IRL Day)
-				SlowTime.this.getServer().getWorlds().get(0).setFullTime((long) (SlowTime.this.getServer().getWorlds().get(0).getFullTime() - 67L);
+				SlowTime.this.getServer().getWorlds().get(0).setFullTime((long) (SlowTime.this.getServer().getWorlds().get(0).getFullTime() - 67L));
 			}
 			
 		}, 72L, 72L);
